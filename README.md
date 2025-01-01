a78a3668b48128e48ba8d52f57bf7c080f7fb5a1
72umesh <umeshpal8141@gmail.com>
2025-01-01 13:59:22 +0530

    First Commit Project Complete

---
+++ README.md
@@ -0,0 +1,15 @@
+# Frontend Developer Portfolio
+
+Welcome to my portfolio! I'm a passionate and dedicated frontend developer. Focus on delivering high-quality, user-friendly websites and web applications. Below you'll find information about my skills, experience, and projects that showcase my work.
+
+## About Me
+
+Hi! I'm Umesh, a frontend developer building responsive and interactive websites. I specialize in HTML, CSS, JavaScript, and modern frontend frameworks like React. I love creating seamless user experiences and clean, maintainable code.
+
+## Skills
+
+- **Languages**: HTML, CSS, JavaScript
+- **Frameworks & Libraries**: React, Bootstrap
+- **Version Control**: Git, GitHub
+- **Tools & Software**: VS Code, Figma, Chrome DevTools
+- **Others**: Responsive Design
---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+// Projects 
+// Project 1 
+document.getElementById("project1-github").onclick = function(){
+    location.href="https://github.com/72umesh/Maharaja-Restaurant";
+}
+document.getElementById("project1-demo").onclick = function(){
+    location.href="https://72umesh.github.io/Maharaja-Restaurant/";
+}
+document.getElementById("project-img1").onclick = function(){
+    location.href="https://72umesh.github.io/Maharaja-Restaurant/";
+}
+
+// Project 2
+document.getElementById("project2-github").onclick = function(){
+    location.href="https://github.com/72umesh/StoreX";
+}
+document.getElementById("project2-demo").onclick = function(){
+    location.href="https://72umesh.github.io/StoreX/";
+}
+document.getElementById("project-img2").onclick = function(){
+    location.href="https://72umesh.github.io/StoreX/";
+}
+
+// Project 3
+document.getElementById("project3-github").onclick = function(){
+    location.href="https://github.com/72umesh/To-Do-List-React";
+}
+document.getElementById("project3-demo").onclick = function(){
+    location.href="https://72umesh.github.io/To-Do-List-React/";
+}
+document.getElementById("project-img3").onclick = function(){
+    location.href="https://72umesh.github.io/To-Do-List-React/";
+}
+
+
